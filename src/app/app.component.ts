528f1a29441763e32e4ee6ab089c3d3ae07caee7
lml5 <gustavo.cisnerosmatias@gmail.com>
2026-04-11 12:36:06 -0500

    1 version portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Certificado } from './entities/certificados.entities';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portafolio';
+
+  certificados: Certificado[] = [
+    {
+      id: '1',
+      description: 'Programacion desde cero',
+      plataforma: 'EDteam',
+      img: './resources/programcionCero_certificado.jpeg',
+    },
+    {
+      id: '2',
+      description: '¿Como Funciona La Tecnologia Computadoras?',
+      plataforma: 'EDteam',
+      img: './resources/ComoFuncionaLaTecnologiaComputadoras.jpeg',
+    },
+    {
+      id: '3',
+      description: '¿Como Funciona La Tecnologia Software?',
+      plataforma: 'EDteam',
+      img: './resources/ComoFuncionaLaTecnologiaSoftware.jpeg',
+    },
+  ];
 }
---
+++ src/app/entities/certificados.entities.ts
@@ -0,0 +1,6 @@
+export interface Certificado {
+  id: string;
+  description: string;
+  plataforma: string;
+  img: string;
+}
